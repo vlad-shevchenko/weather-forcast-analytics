cbb3fb53c2d97ef7cff132e178e07d162e791b5d
Vlad Shevchenko <vlad.shevchenko.ua@gmail.com>
2018-02-23 19:26:02 +0200

    Initial commit

---
+++ src/main/java/weather/ForecastAnalyticsApplication.java
@@ -0,0 +1,12 @@
+package weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastAnalyticsApplication.class, args);
+	}
+}
---
+++ src/test/java/weather/ForecastAnalyticsApplicationTests.java
@@ -0,0 +1,16 @@
+package weather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForecastAnalyticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
